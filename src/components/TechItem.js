394c1607109b31048d061d3ce912a62c900eacba
Fabio Moraes <fabio@elaboredigital.com.br>
2019-09-02 19:18:06 -0300

    React Curso

---
+++ src/components/TechItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function TechItem({ tech, onDelete }) {
+  return (
+    <li>
+      { tech }
+      <button onClick={onDelete} type="button">Remover</button>
+    </li>
+  );
+}
+
+TechItem.defaultProps = {
+  tech: 'Oculto',
+}
+
+TechItem.PropTypes = {
+  tech: PropTypes.string,
+  onDelete: PropTypes.func.isRequired,
+}
